24b17066c7c42ffda3a7f711df15d342e44410c4
Peer Maute <peer.maute@haw-hamburg.de>
2022-05-02 10:07:37 +0200

    initial commit

---
+++ src/main/java/dev/peermaute/mealsquare/MealSquareApplication.java
@@ -0,0 +1,13 @@
+package dev.peermaute.mealsquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealSquareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealSquareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/peermaute/mealsquare/MealSquareApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.peermaute.mealsquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealSquareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
