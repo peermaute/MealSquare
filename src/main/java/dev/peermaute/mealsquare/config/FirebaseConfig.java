270d8cdf0a43e3f0b28193f2f936c16bf49b484c
Peer Maute <peer.maute@haw-hamburg.de>
2022-11-07 08:13:02 +0100

    created new service account with new credentials

--- src/main/java/dev/peermaute/mealsquare/config/FirebaseConfig.java
+++ src/main/java/dev/peermaute/mealsquare/config/FirebaseConfig.java
@@ -19,8 +19,6 @@ public class FirebaseConfig {
     @Primary
     @Bean
     public void firebaseInit() throws IOException {
-        JSONObject firebaseConfig = initiateFirebaseConfigJson();
-        String fireBaseConfigString = getFirebaseConfig();
         InputStream stream = new ByteArrayInputStream(getFirebaseConfig().getBytes(StandardCharsets.UTF_8));
         FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(stream)).build();
         FirebaseApp.initializeApp(options);
@@ -46,7 +44,7 @@ public class FirebaseConfig {
         firebaseConfig.put("type", "service_account");
         firebaseConfig.put("project_id", System.getenv("FIREBASE_CONFIG_PROJECT_ID"));
         firebaseConfig.put("private_key_id", System.getenv("FIREBASE_CONFIG_PRIVATE_KEY_ID"));
-        firebaseConfig.put("private_key", System.getenv("-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC9G4jXyW2vQF0z\n9x9eVRUc3/jRLEaKq0vZ+v6SRdTePIxIsUTgIyWMkGjAYFKSFEleEoxEwez/XXeX\nsqDsUK51RVBZVAoFE55n2gJXO+Z+QsVDqmaFiHXNBEnZcQ9N1rDd6Q3gKmrLbQEN\nsBjnoHzdaW0rHZZMSJeBWWv7WCiUmm5MFUi31B4Q1Q4UWRTDvgsQexYDl9TqBfvf\nmMrVovGNNCH1FXeWJuQ6rXoMQbswGDJRaHAxkDmSX41cPDM8zWKWVDvMIUt9gSVY\nYh1cWMmyoBcaSXiEUqwX7PnQoxOIUpLGPpdvDWRtOSq8z4BmLsbGDDS9V5mA7UPN\no5YH/POTAgMBAAECggEAGhgZD1w/kthTpA6HA7oYzOBePJAaocWA6htiElFUI2BW\neSy2KiQfaSB/cSrkFJJ7tzaMUsExpeEXVgAziA2arRrl5erx66u+hr+vOqtMza0C\npv4gjG99KyTa2IgVylyuu3LjqRl2nWBgA1atG7+/oGu3lnn22crbK7bo7rNGUjl/\nTpz9N/9grSeEZNtO1HBcnxeAF6dlxS+w1namGmD+ywp/pAIjV34tvsldmOCOzGtA\n1Hwek0bGlvMw1xVYcBsWKWVPh/ShBRWeXwmviGPSi1EWE71GhMaINVtKId8fDXGD\nYHqBvVhX6W2VX1JtD9XcONHBDUlwDaNIAkPJbMzJ+QKBgQD0K1r0Qb2OHdOkPpJr\nAfs+nNcv4mSC4xvtKL9kLQGfLWKgRryuRu2kjaMnsPoeGOFBpRtWK1xtujpNjyjL\nf37/OJn9Rd6MY3XCFzC/qxeH3Yv341oFaqcEgUMP17B7fCcBfsgPyKxB2nHO/xnd\nD0BKYMhIbrT1yrL+rMrJYgceGQKBgQDGRTMsyXzKFvFPI4stNeuNTkr9iCjNGe3r\nwhQmKBi6QugvpG+Ei0QHEHZBqpxC0djz+UTnk4+oLuO02YMf8gLzvtwsm2JHQD48\nbfrx/qGd9EkD1LVcw2c0acQoChOePsxiev4nvo18ykTB25mJGXdbUQevlXuWZl+g\n7C3+Ylf8iwKBgGxCtOdMJsHWk7plTAJKNJ4EaJ0AolQuYnl68bF3CxsKwooSEyKq\nrD+j1U2J+LxTh7zVehNw0mCMi8FL0mi4NRTwgcZ8rjpM9y+BnMm/EIXbQ247zfRM\n35Ttw1BI8CUwXRMapZXqAgaI1qFTFqKfWoAf/W2wOGgq4cZt0tQLtOAxAoGBALnW\nFKBO/nMgy+8kXofyuSTKFInMPIn5WsKWN3KINJ0opx44nvn52rpQWLc15Z7Pt+z9\nX/M6oPS/dliFY1zm5pN1+kKpJZWpBmx3nTWDOBKn7GP11EPtNueultf8keTJ8aIA\nrAWumkk0APw/C4qwXuotaenOT3dp3wojNPzxSoz/AoGAe07QNX4YDSCE4lLm3pjQ\nlX36nUiSZCpQMpiDxwGJaLDteZqQsDx0DXEgulP43M+z/QbcLcax0VCykOmFOtIh\n32WVmojV+zwp8etFlaGfUdq1MZlrR4IhlAWGTi05qDiJK+pXsckLmToGLsVsDX+N\ndcMPVhap0dYZ5/SoBTKLqpE=\n-----END PRIVATE KEY-----\n"));
+        firebaseConfig.put("private_key", System.getenv("FIREBASE_CONFIG_PRIVATE_KEY"));
         firebaseConfig.put("client_email", System.getenv("FIREBASE_CONFIG_CLIENT_EMAIL"));
         firebaseConfig.put("client_id", System.getenv("FIREBASE_CONFIG_CLIENT_ID"));
         firebaseConfig.put("auth_uri", "https://accounts.google.com/o/oauth2/auth");
